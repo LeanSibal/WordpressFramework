ef90bffd956d5ee2b28d40fc352b09011e6daaae
Lean Sibal <leansibal@gmail.com>
2016-05-25 20:07:35 +0800

    Initial Plugin commit

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WordpressPlugin
+ * @version 0.0
+ */
+/*
+Plugin Name: Wordpress Plugin
+Plugin URI: https://github.com/LeanSibal/WordpressFramework
+Description: Wordpress abstraction framework Plugin for Web Developers
+Author: Leandro Sibal
+Version: 0.0
+Author URI: https://github.com/LeanSibal
+*/
+
